3abfa11a745be02308ac4104aa960d2dead32869
Alan Cole <me@alancole.io>
2019-06-18 17:19:22 +0100

    File layout

---
+++ tests/LibraTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Libra\Tests;
+
+class LibraTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Blank test
+     */
+    public function testWeAreAlive()
+    {
+        $this->assertTrue(true);
+    }
+}
